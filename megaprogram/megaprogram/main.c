90718b73f9534405a6d16badaa81d241d678291f
indietom <tom.leo@hotmail.com>
2018-08-16 16:54:56 +0200

    avg, delay

--- megaprogram/megaprogram/main.c
+++ megaprogram/megaprogram/main.c
@@ -39,14 +39,46 @@ int main (void)
 	
 	UARTInit();
 	
+	char toPrint[3];
+	uint8_t delay = 0;
+	
+	char avgBuffer[64];
+	uint8_t avgIndex;
+	char avgChar;
+	
 	while (1)
 	{
 		LCDWriteCommand(0x80);
 		LCDWriteCommand(0x40);
 		
-		LCDWriteStr("WHAT I GET: ");
-		LCDWriteChr(UARTGet());
+		LCDWriteStr("WHAT I GET:");
 		
+		uint8_t i = 0;
+		delay = delay + 1;
+		if(delay >= 128/4)
+		{
+			for(i = 0; i < 3; i++) 
+			{
+				toPrint[i] = UARTGet();
+				if(avgIndex < 64) avgBuffer[avgIndex++] = toPrint[i]; 
+			}
+			delay = 0;
+		}
+		
+		for(i = 0; i < 3; i++) LCDWriteChr(toPrint[i]);//UARTGet());
+		
+		LCDWriteStr("AVG:");
+		if(avgIndex >= 64-1) 
+		{
+			uint16_t sum;
+			for(i = 0; i < 64; i++)
+			{
+				sum = sum + avgBuffer[i];
+			}
+			avgChar = sum/64;
+			avgIndex = 0;
+		}
+		LCDWriteChr(avgChar);
 		LCDWriteCommand(0x80);
 		LCDWriteCommand(0x40);
 		LCDClear();
